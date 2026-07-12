b2fbeebca646403b35335c81ec70d08ff3054460
Shamkumar <shyamkumar64077@gmail.com>
2026-07-12 21:47:34 +0530

    Delete Lesson.java

--- Lesson.java
+++
@@ -1,5 +0,0 @@
-package com.sham.studylauncher;
-final class Lesson {
- final int day, hour, minute; final String code, url;
- Lesson(int day,int hour,int minute,String code,String url){this.day=day;this.hour=hour;this.minute=minute;this.code=code;this.url=url;}
-}
